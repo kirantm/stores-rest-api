c71e2f0e0e4e8452d74dae6fb289556c818506cc
Kiran Thapa <kirantm2020@gmail.com>
2022-06-16 13:23:57 -0500

    First commit, added REST API to access items, stores, and user authentication.

---
+++ models/item.py
@@ -0,0 +1,41 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel') #Single instance of StoreModel
+
+    def __init__(self, name, price, store_id):
+        self.name = name
+        self.price = price
+        self.store_id = store_id
+
+    def json(self):
+        return {'name': self.name, 'price': self.price}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name LIMIT 1
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # def update(self):
+    #     conn = sqlite3.connect('data.db')
+    #     cur = conn.cursor()
+    #
+    #     query = "UPDATE items SET price=? WHERE name=?"
+    #     cur.execute(query, (self.price, self.name))
+    #
+    #     conn.commit()
+    #     conn.close()
---
+++ models/store.py
@@ -0,0 +1,37 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    items = db.relationship('ItemModel', lazy='dynamic') # List of ItemModel instances
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first() #SELECT * FROM items WHERE name=name LIMIT 1
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    # def update(self):
+    #     conn = sqlite3.connect('data.db')
+    #     cur = conn.cursor()
+    #
+    #     query = "UPDATE items SET price=? WHERE name=?"
+    #     cur.execute(query, (self.price, self.name))
+    #
+    #     conn.commit()
+    #     conn.close()
